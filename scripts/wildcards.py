e1b496a92761f1db0b3b09368fa511be682f89f4
uorufu <153657365+uorufu@users.noreply.github.com>
2024-04-05 07:19:27 +0200

    Update wildcards.py

--- scripts/wildcards.py
+++ scripts/wildcards.py
@@ -138,7 +138,7 @@ Save your wildcards in the wildcards folder. To avoid issues, use only a-z in yo
                         if line.startswith(str(x) + "_"):
                             if x > 9:
                                 line = line[3:]
-                            else
+                            else:
                                 line = line[2:]
                             sendline = line
                             text[i] = self.replace_wildcard(sendline, rand_list_tiers[x])
